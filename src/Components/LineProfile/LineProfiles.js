8239a997089ceb00a669fa19a3c6f70b73365482
Ryan <ryanscottmckay@gmail.com>
2020-05-25 20:29:30 -0600

    Added color swatch to the profile component

--- src/Components/LineProfile/LineProfiles.js
+++ src/Components/LineProfile/LineProfiles.js
@@ -7,25 +7,31 @@ const LineProfile = (props) => {
     const line = props.line;
     return (
         <div className={styles.LineProfile} key={uuid()}>
-            <div className={styles.topSection}>
-
-            </div>
-
-            <hr/>
-
-            <div className={styles.bottomSection}>
-                <a>Length: </a>
-                <div className={styles.numberContainer}>
-                    <h5 className={styles.number}>{line.length}</h5>
+            <div className={styles.leftSide}>
+                <div className={styles.swatch} style={{backgroundColor: 'red'}}>
                 </div>
-                {line.angle ? (
-                    <React.Fragment>
-                        <a>Angle</a>
-                        <div className={styles.numberContainer}>
-                            <h5 className={styles.number}>{line.angle.toFixed(1)}</h5>
-                        </div>
-                    </React.Fragment>
-                ) : null}
+            </div>
+            <div className={styles.rightSide}>
+                <div className={styles.topSection}>
+
+                </div>
+
+                <hr/>
+
+                <div className={styles.bottomSection}>
+                    <a>Length: </a>
+                    <div className={styles.numberContainer}>
+                        <h5 className={styles.number}>{line.length}</h5>
+                    </div>
+                    {line.angle ? (
+                        <React.Fragment>
+                            <a>Angle</a>
+                            <div className={styles.numberContainer}>
+                                <h5 className={styles.number}>{line.angle.toFixed(1)}</h5>
+                            </div>
+                        </React.Fragment>
+                    ) : null}
+                </div>
             </div>
         </div>
     )
