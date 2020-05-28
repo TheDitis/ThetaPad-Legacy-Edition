6e28d4c0f3877254ec40f9f819e82f6b87f15a54
Ryan <ryanscottmckay@gmail.com>
2020-05-27 22:33:29 -0600

    moved length markers of StraightLine to middle

--- src/Components/Lines/StraightLine.js
+++ src/Components/Lines/StraightLine.js
@@ -4,14 +4,34 @@ import {Line, Text} from "react-konva";
 
 
 const StraightLine = (props) => {
+
+    const line = props.line;
     const points = [props.x1 - props.widthSub, props.y1, props.x2 - props.widthSub, props.y2];
+
+
+    const findMidPoint = (a, b) => {
+        const [x1, y1] = a;
+        const [x2, y2] = b;
+
+        const xMid = (x1 + x2) / 2 - props.widthSub;
+        const yMid = (y1 + y2) / 2;
+
+        return [xMid, yMid]
+    };
+
+    const [xMid, yMid] = findMidPoint([props.x1, props.y1], [props.x2, props.y2]);
+
+    console.log(props.widthSub);
+    console.log("pt1:", props.x1, props.y1, "pt2", props.x2, props.y2);
+    console.log("midpoint: ", xMid, yMid);
+
     return (
         <React.Fragment>
             <Line key={uuid()} x={0} y={0} stroke={props.color} points={points} strokeWidth={2}/>
             <Text
                 text={(props.length / props.unit).toFixed(2)}
-                x={props.x1 - props.widthSub}
-                y={props.y1 + 10}
+                x={xMid - 22}
+                y={yMid + 10}
                 rotation={props.angles[0]}
                 fontSize={15}
                 fill={props.color}
